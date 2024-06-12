5bb73cd2fece15b2a1df9546527468f1f06a50c6
Deepak <deepakjack.dj@gmail.com>
2024-06-12 16:17:22 +0530

    first commit

---
+++ src/components/Aboutus.js
@@ -0,0 +1,33 @@
+import "./AboutusStyles.css";
+
+function AboutUs() {
+  return (
+    <div className="about-container">
+      <h1>Our History </h1>
+      <p>
+        Tripzzz is owned and managed by Tripzzz .in Pvt Ltd. a leading brand in
+        web designing services and e-commerce solutions. Trippzz .In Pvt Lts is
+        counted for its expertise in web solutions and its top ranking business
+        portals. Our invisible expertise and rich experience has raised our
+        spirit to reach ahead from our client's expectation. Commendable success
+        rate of other portals managed by Tripzzz is alive paradigm of our work
+        excellence.
+      </p>
+
+      <h1> Our Mission </h1>
+      <p>
+        Our Mission is to touch the horizon where our capabilities may
+        successfully meet with the requirements of our clients.that too with
+        ultimate transparency and cost-effectiveness.
+      </p>
+
+      <h1> Our Vision</h1>
+      <p>
+        To saw the seeds of par-excellence services with customer centric
+        approach and reap the trust of worldwide clients.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
